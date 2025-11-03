8e066dce17d1722001743de10661717b3044e040
sabahali <sabahalick21@gmail.com>
2025-11-04 00:19:11 +0530

    login

--- netlify/functions/test.ts
+++ netlify/functions/test.ts
@@ -7,16 +7,6 @@ dotenv.config({ override: false });
 
 export default async (req: Request, context: Context) => {
 
-// const credentialsPath =process.env.NODE_ENV === 'dev' ? path.join(current_dir, '../../etc/secrets/credentials.json') : 
-// path.join(current_dir, '../../etc/secrets/credentials.json');
-  // process.env.NODE_ENV === 'dev'
-  //   ? path.resolve('/etc/secrets/credentials.json') // absolute path in prod
-  //   : path.join(__dirname, '../../etc/secrets/credentials.json'); // local path for dev
-
-// const rawData = fs.readFileSync(credentialsPath, 'utf-8');
-// const serviceAccount = JSON.parse(rawData);
-// const serviceAccountEnv=JSON.parse(process.env.SERVICE_ACCOUNT as string)
-
 const serviceAccountObj :any = {
   type : process.env.type,
   project_id : process.env.project_id,
@@ -30,6 +20,6 @@ const serviceAccountObj :any = {
   client_x509_cert_url : process.env.client_x509_cert_url,
   universe_domain : process.env.universe_domain
 }
-console.log(serviceAccountObj.private_key)
+
   return new Response(serviceAccountObj.private_key)
 }
